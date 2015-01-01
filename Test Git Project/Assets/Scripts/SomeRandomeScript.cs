843d81e154cc63dd7b6ac042c15ec649238cd26f
Kevin Phung <phung.kevin@gmail.com>
2014-12-31 20:02:26 -0800

    made some new changes and added some new assets

---
+++ Test Git Project/Assets/Scripts/SomeRandomeScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SomeRandomeScript : MonoBehaviour {
+
+	private int aNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		aNumber += 1;
+
+		if (aNumber % 20 == 0) {
+			Debug.Log("Ya dude we got 20");
+		}
+
+	}
+
+}
